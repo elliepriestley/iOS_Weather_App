79ba79c538ec38011905942405f36d820d94ef74
elliepriestley <ellie.priestley@gmail.com>
2023-10-06 17:11:46 +0100

    Successful testing of getting coordinates from user - manual testing using iphone

--- WeatherApp/Views/LoadingView.swift
+++ WeatherApp/Views/LoadingView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct LoadingView: View {
     var body: some View {
         ProgressView()
-            .progressViewStyle(CircularProgressViewStyle(tint: white))
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
