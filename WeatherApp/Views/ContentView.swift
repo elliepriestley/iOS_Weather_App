886caf30752b693841f447c35f9dc5b2fa602539
elliepriestley <ellie.priestley@gmail.com>
2023-10-06 16:49:26 +0100

    Built ContentView, WelcomeView, started on LoadingView

---
+++ WeatherApp/Views/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Ellie Priestley on 06/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var locationManager = LocationManager()
+    
+    var body: some View {
+        VStack {
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            } else {
+                if locationManager.isLoading {
+                    ProgressView()
+                } else {
+                    WelcomeView()
+                        .environmentObject(locationManager)
+                }
+            }
+            
+          
+        }.background(Color(hue: 0.661, saturation: 0.631, brightness: 0.456)).preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/Views/LoadingView.swift
@@ -0,0 +1,19 @@
+//
+//  LoadingView.swift
+//  WeatherApp
+//
+//  Created by Ellie Priestley on 06/10/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: white))
+    }
+}
+
+#Preview {
+    LoadingView()
+}
